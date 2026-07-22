ecaad878ab7544842d7f6e7090417dec29bc6d33
Matteo Somensi <matteo.somensi@gullivernet.com>
2026-07-22 11:02:13 +0200

    fix: restore feature modules compose dependencies and correct namespaces

--- feature_pokemon_detail/build.gradle.kts
+++ feature_pokemon_detail/build.gradle.kts
@@ -3,13 +3,21 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pokedex.feature.pokemondetail"
+    namespace = "com.example.pokedex.featurepokemondetail"
 }
 
 dependencies {
     implementation(project(":domain"))
     implementation(project(":core"))
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.coil.compose)
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.iconsCore)
+    implementation(libs.androidx.compose.ui.tooling.preview)
 }
